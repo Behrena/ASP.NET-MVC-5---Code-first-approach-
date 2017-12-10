067f6563366fd764e25f699fde4d8e374eb679dd
Behri <Behrij@hotmail.com>
2017-12-10 14:57:12 -0500

    Movie Project

---
+++ source/repos/MvcMovie/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+      
+        public string Welcome(string name,int numTimess = 1)
+        {
+            // HttpUtility.HtmlEncode to protect the application from malicious input(namely JavaScript)
+            return HttpUtility.HtmlEncode("Hello" + name + ", NumTime is: " + numTimess);
+                //"This is the Welcome action method...";
+        }
+    }
+}
